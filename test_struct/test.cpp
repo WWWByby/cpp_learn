ea4ddaa572dc2dc1c798abcec023aff64590462d
Snake <18530895881@163.com>
2018-11-25 07:25:32 -0800

    结构体使用和运算符重载

--- test_struct/test.cpp
+++ test_struct/test.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+using namespace std;
+struct manStrct
+{
+	std::string name;
+
+	int price;
+	
+	manStrct()
+	{
+		name = "未初始化";
+		price = 0;
+	}
+
+	friend ostream & operator<< (ostream &out ,manStrct  &str)
+	{
+		out << "名字：" << str.name << "price："<<str.price << std::endl;
+		return out ;
+	}
+};
+
+void test_struct()
+{
+	std::cout << "测试结构体" << std::endl;
+	manStrct man;
+	
+	cout << man <<endl;
+
+	man.name = "我是测试体";
+
+	man.price = 100;
+	
+	std::cout << man <<std::endl;
+}
 
 int main(void)
 {
-    std::cout << "test"<< std::endl;
-    return 0;
+	test_struct();
+	return 0;
 }
